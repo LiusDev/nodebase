b342e6f528fd7196bb9aa02899ff9b9863f0c5f4
LiusDev <quydx.work@gmail.com>
2025-11-16 17:33:11 +0700

    Add TRPC setup and client integration with React Query
    - Updated package.json and package-lock.json to include TRPC and React Query dependencies.
    - Implemented TRPC context and router in init.ts and _app.ts.
    - Created query client management in query-client.ts.
    - Developed server-side logic in server.ts for TRPC.
    - Added client component for rendering in page.tsx.
    - Established API route handling in route.ts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-	return <div>nice</div>
+import { getQueryClient, trpc } from "@/trpc/server"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import Client from "./client"
+
+export default async function Page() {
+	const queryClient = getQueryClient()
+
+	await queryClient.prefetchQuery(
+		trpc.hello.queryOptions({ text: "from tRPC" })
+	)
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<Client />
+		</HydrationBoundary>
+	)
 }
