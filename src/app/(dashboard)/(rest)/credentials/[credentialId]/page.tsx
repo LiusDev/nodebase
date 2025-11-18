198492c8c96eaece9f3ae3802fc7b129a8a8494a
LiusDev <quydx.work@gmail.com>
2025-11-18 14:58:31 +0700

    feat: layouts

---
+++ src/app/(dashboard)/(rest)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,11 @@
+import { requireAuth } from "@/lib/auth-utils"
+
+interface PageProps {
+	params: Promise<{ credentialId: string }>
+}
+
+export default async function Page({ params }: PageProps) {
+	await requireAuth()
+	const { credentialId } = await params
+	return <div>Credential ID Page {credentialId}</div>
+}
---
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -0,0 +1,11 @@
+import { requireAuth } from "@/lib/auth-utils"
+
+interface PageProps {
+	params: Promise<{ executionId: string }>
+}
+
+export default async function Page({ params }: PageProps) {
+	await requireAuth()
+	const { executionId } = await params
+	return <div>Execution ID Page {executionId}</div>
+}
